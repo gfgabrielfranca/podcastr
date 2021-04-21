1bc0f69bbf5ea6ce994a2b64c4a97c3dffc26561
Gabriel França <dev.gabrielfranca@gmail.com>
2021-04-21 20:40:15 -0300

    aula 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+export default function Home(props) {
+  return (
+    <p>{JSON.stringify(props.episodes)}</p>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+}
